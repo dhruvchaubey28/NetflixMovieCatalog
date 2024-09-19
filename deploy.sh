25be5a48ebd55f27d170ca2dfaaab0a45ec7b256
Dhruv Chaubey <143328600+dhruvchaubey28@users.noreply.github.com>
2024-09-19 15:49:54 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
 
-# TODO your deploy script implementation...
+sudo systemctl stop simplepy.service
+
+sudo systemctl enable simplepy.service
+
+sudo systemctl start simplepy.service
+
+
+sudo systemctl start nginx
